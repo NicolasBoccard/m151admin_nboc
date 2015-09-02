3e2fd4ede2eeb69565ad4604ebf4ed0d7d14cc1b
NicolasBoccard <nico.boccard.cfpt@gmail.com>
2015-09-02 13:15:10 +0200

    Test sur le fonctionnement de la connexion

--- m151admin/index.php
+++ m151admin/index.php
@@ -11,6 +11,11 @@ if(isset($_POST['inscription']))
     inscription($_POST['nom'], $_POST['prenom'], $_POST['date_naissance'], $_POST['description'], $_POST['email'], $_POST['pseudo'], $_POST['mot_de_passe']);
 }
 
+if(isset($_POST['effacer']))
+{
+    connexion();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -25,20 +30,27 @@ if(isset($_POST['inscription']))
             <form  method="POST" action="index.php">
                 <label for="nom">Nom :</label>
                 <input type="text" id="nom" name="nom"/><br/>
+                
                 <label for="prenom">Prénom :</label>
                 <input type="text" id="prenom" name="prenom"/><br/>
+                
                 <label for="date_naissance">Date de naissance :</label>
-                <input type="text" id="date_naissance" name="date_naissance"></textarea><br/>
+                <input type="date" id="date_naissance" name="date_naissance"></textarea><br/>
+                
                 <label for="description">Description :</label>
                 <textarea id="description" name="description"></textarea><br/>
+                
                 <label for="email">Email :</label>
                 <input type="email" id="email" name="email"/><br/>
+                
                 <label for="pseudo">Pseudo :</label>
                 <input type="text" id="pseudo" name="pseudo"/><br/>
+                
                 <label for="mot_de_passe">Mot de passe :</label>
                 <input type="password" id="mot_de_passe" name="mot_de_passe"/><br/>
+                
                 <input type="submit" name="inscription" value="Inscription"/>
-                <input type="reset" name="effacer" value="Effacer"/>
+                <input type="submit" name="effacer" value="Effacer"/>
             </form>
         </div>
     </body>
